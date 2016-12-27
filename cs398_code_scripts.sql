aa01b1b53f15349c42f5fbe99099583def2e8b37
Ekrem Çetinkaya <ekrem.cetinkaya@ozu.edu.tr>
2016-12-27 17:41:55 +0300

    Updated triggers, finalizing business rules

--- cs398_code_scripts.sql
+++ cs398_code_scripts.sql
@@ -32,7 +32,7 @@ BEGIN
 END;
 
 CREATE OR REPLACE TRIGGER project_leader_correct_trigger
-BEFORE INSERT OR UPDATE OF LEADER_ID ON OZU_PROJECTS
+AFTER INSERT OR UPDATE OF LEADER_ID ON OZU_PROJECTS
 FOR EACH ROW
 DECLARE
   v_department_id OZU_EMPLOYEES.DEPARTMENT_ID%TYPE;
@@ -45,10 +45,12 @@ BEGIN
   SELECT HEAD_ID INTO v_manager_id FROM OZU_DEPARTMENTS
   WHERE DEPARTMENT_ID = v_department_id;
 
-  SELECT EMPLOYEE_ID INTO v_exists FROM OZU_PROJECT_TEAMS
-  WHERE PROJECT_ID = :NEW.PROJECT_ID AND EMPLOYEE_ID = v_manager_id;
-  EXCEPTION WHEN NO_DATA_FOUND THEN
-    v_exists := 0;
+  BEGIN
+    SELECT EMPLOYEE_ID INTO v_exists FROM OZU_PROJECT_TEAMS
+    WHERE PROJECT_ID = :NEW.PROJECT_ID AND EMPLOYEE_ID = v_manager_id;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      v_exists := 0;
+  END;
 
   IF (v_exists != 0) THEN
     RAISE_APPLICATION_ERROR(-20503,'A MEMBER CANNOT BE PROJECT LEADER IF HIS/HER MANAGER IS ALSO IN THE TEAM');
@@ -60,7 +62,6 @@ BEFORE DELETE OR INSERT OR UPDATE OF EMPLOYEE_ID,PROJECT_ID ON OZU_PROJECT_TEAMS
 FOR EACH ROW
 DECLARE
   v_old_project_status OZU_PROJECTS.PROJECT_STATUS%TYPE := 'A';
-  v_project_check NUMBER := 0;
   v_new_project_status OZU_PROJECTS.PROJECT_STATUS%TYPE;
   v_member_count OZU_EMPLOYEES.MEMBERSHIP_COUNT%TYPE;
 BEGIN
@@ -70,73 +71,69 @@ BEGIN
       WHERE PROJECT_ID = :OLD.PROJECT_ID;
     END IF;
 
-    BEGIN
-      SELECT DISTINCT COUNT(EMPLOYEE_ID) FROM OZU_PROJECT_TEAMS
-      WHERE PROJECT_ID = :NEW.PROJECT_ID AND EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
-      EXCEPTION WHEN NO_DATA_FOUND THEN
-        v_project_check := 0;
-    END;
-
     SELECT PROJECT_STATUS INTO v_new_project_status FROM OZU_PROJECTS
     WHERE PROJECT_ID = :NEW.PROJECT_ID;
 
     SELECT MEMBERSHIP_COUNT INTO v_member_count FROM OZU_EMPLOYEES
     WHERE EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
-    IF(v_project_check = 0) THEN
-      IF (v_old_project_status IN ('S','P') AND v_new_project_status IN ('C','F')) THEN
-        v_member_count := v_member_count - 1;
-      ELSIF (v_new_project_status IN ('C','F')) THEN
-        v_member_count := v_member_count;
-      ELSE
-        v_member_count := v_member_count + 1;
-      END IF;
+    IF (v_old_project_status IN ('S','P') AND v_new_project_status IN ('C','F')) THEN
+       v_member_count := v_member_count - 1;
+    ELSIF (v_new_project_status IN ('C','F')) THEN
+      v_member_count := v_member_count;
+    ELSE
+      v_member_count := v_member_count + 1;
     END IF;
 
-    IF(v_member_count > 5 ) THEN
+    IF(v_member_count > 5) THEN
       RAISE_APPLICATION_ERROR(-20503,'AN EMPLOYEE CANNOT BE MEMBER OF MORE THAN 5 ACTIVE PROJECTS');
     ELSE
       UPDATE OZU_EMPLOYEES SET MEMBERSHIP_COUNT = v_member_count WHERE EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
     END IF;
     /* If deleting part */
-    /* TODO Delete kısmında projeden mi yoksa tasktan mı silindi onu anlamak lazım düzelt */
-    ELSE
-      BEGIN
-        SELECT DISTINCT COUNT(EMPLOYEE_ID) FROM OZU_PROJECT_TEAMS
-        WHERE PROJECT_ID = :OLD.PROJECT_ID AND EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
-        EXCEPTION WHEN NO_DATA_FOUND THEN
-          v_project_check := 0;
-      END;
-      IF(v_project_check = 0) THEN
-        SELECT MEMBERSHIP_COUNT INTO v_member_count FROM OZU_EMPLOYEES
-        WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
-        v_member_count := v_member_count - 1;
-        UPDATE OZU_EMPLOYEES SET MEMBERSHIP_COUNT = v_member_count WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
-      END IF;
+  ELSE
+    SELECT MEMBERSHIP_COUNT INTO v_member_count FROM OZU_EMPLOYEES
+    WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
+    v_member_count := v_member_count - 1;
+    UPDATE OZU_EMPLOYEES SET MEMBERSHIP_COUNT = v_member_count WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
   END IF;
 END;
 
 CREATE OR REPLACE TRIGGER work_percentage_trigger
-BEFORE INSERT OR UPDATE OF WORK_PERCENTAGE ON OZU_PROJECT_TEAMS
+BEFORE DELETE OR INSERT OR UPDATE OF WORK_PERCENTAGE ON OZU_PROJECT_TEAMS
 FOR EACH ROW
 DECLARE
+  v_per_difference OZU_EMPLOYEES.TOTAL_WORK_PERCENTAGE%TYPE := :NEW.WORK_PERCENTAGE;
   v_work_per OZU_EMPLOYEES.TOTAL_WORK_PERCENTAGE%TYPE;
   v_project_status OZU_PROJECTS.PROJECT_STATUS%TYPE;
 BEGIN
-  SELECT TOTAL_WORK_PERCENTAGE INTO v_work_per FROM OZU_EMPLOYEES
-  WHERE EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
-  SELECT PROJECT_STATUS INTO v_project_status FROM OZU_PROJECTS
-  WHERE PROJECT_ID = :NEW.PROJECT_ID;
-  IF(v_project_status IN ('P','S')) THEN
-    v_work_per := v_work_per + :NEW.WORK_PERCENTAGE;
-  END IF;
-  IF(v_work_per > 1) THEN
-    RAISE_APPLICATION_ERROR(-20505,'TOTAL WORK PERCENTAGE OF EMPLOYEE CANNOT BE MORE THAN 1');
+  IF NOT DELETING THEN
+    SELECT TOTAL_WORK_PERCENTAGE INTO v_work_per FROM OZU_EMPLOYEES
+    WHERE EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
+    SELECT PROJECT_STATUS INTO v_project_status FROM OZU_PROJECTS
+    WHERE PROJECT_ID = :NEW.PROJECT_ID;
+    IF UPDATING THEN
+      v_per_difference := :NEW.WORK_PERCENTAGE - :OLD.WORK_PERCENTAGE;
+    END IF;
+
+    IF(v_project_status IN ('P','S')) THEN
+      v_work_per := v_work_per + v_per_difference;
+    ELSIF(v_project_status IN ('C','F')) THEN
+      v_work_per := v_work_per;
+    END IF;
+
+    IF(v_work_per > 1 OR v_work_per < 0) THEN
+      RAISE_APPLICATION_ERROR(-20505,'TOTAL WORK PERCENTAGE OF EMPLOYEE CANNOT BE MORE THAN 1, AND SMALLER THAN 0');
+    ELSE
+      UPDATE OZU_EMPLOYEES SET TOTAL_WORK_PERCENTAGE = v_work_per WHERE EMPLOYEE_ID = :NEW.EMPLOYEE_ID;
+    END IF;
+  /* deleting part*/
   ELSE
-    UPDATE OZU_EMPLOYEES SET TOTAL_WORK_PERCENTAGE = v_work_per;
+    SELECT TOTAL_WORK_PERCENTAGE INTO v_work_per FROM OZU_EMPLOYEES
+    WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
+    v_work_per := v_work_per - :OLD.WORK_PERCENTAGE;
+    UPDATE OZU_EMPLOYEES SET TOTAL_WORK_PERCENTAGE = v_work_per WHERE EMPLOYEE_ID = :OLD.EMPLOYEE_ID;
   END IF;
 END;
-/*TODO DELETE VE UPDATE DÜZENLENECEK, TASK MI UPDATE INSERT EDILEN PROJE MI ONA BAKILACAK
- */
 
 CREATE TABLE OZU_AUDIT_TASKS (
   C_TASK_ID    NUMBER(4),
